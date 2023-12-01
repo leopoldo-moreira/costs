bcf2264ed55269a79a0b843f80b151571d3cbdb2
Leopoldo Moreira <leopoldomoreira.lm@gmail.com>
2023-12-01 11:37:13 -0300

    Aula 18 - 6:25 tempo da video aula

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import NewProject from './components/pages/NewProject'
+
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+        <Link to='/'>Home</Link>
+        <Link to='/contact'>Contanto</Link>
+        <Link to='/company'>Empresa</Link>
+        <Link to='/newproject'>Novo Projeto</Link>
+      </div>
+      <Routes>
+        <Route exact path='/' element={<Home/>}/> 
+        <Route  path='/company' element={<Company/>}/>
+        <Route  path='/contact' element={<Contact/>}/>
+        <Route  path='/newproject' element={<NewProject/>}/>
+      </Routes>
+    </Router>
   );
 }
 
