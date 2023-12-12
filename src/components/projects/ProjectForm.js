ce5e179d2658faa8b577dc17a3e6fc6466585d79
Leopoldo Moreira <leopoldomoreira.lm@gmail.com>
2023-12-12 11:44:54 -0300

    Aula 23 - Conectando com API

--- src/components/projects/ProjectForm.js
+++ src/components/projects/ProjectForm.js
@@ -1,9 +1,23 @@
+import {useState, useEffect} from 'react'
+
 import Input from '../form/Input';
 import Select from '../form/Select';
 import SubmitButton from '../form/SubmitButton';
 import styles from './ProjectForm.module.css'
 
 function ProjectForm({btnText}){
+
+    const [ , setCategories] = useState([]);
+
+    useEffect(()=>{
+        fetch("http://localhost:5000/categories",{
+        method: "GET",
+        headers: {"Content-Type": "application/json"}
+    }).then((resp) => resp.json())
+      .then((data) => {setCategories(data)})
+      .catch((err) => console.log(err))
+    }, [])    
+    
     return (
         <form className={styles.form}>
             <Input
@@ -18,7 +32,7 @@ function ProjectForm({btnText}){
                 name="budget"
                 placeholder="Insira o orçamento total"            
             />           
-            <Select type="category_id" text="Selecione a categoria"/>
+            <Select type="category_id" text="Selecione a categoria" options={categories}/>
             <SubmitButton text={btnText}/>
         </form>
     )
