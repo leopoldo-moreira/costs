74f58545d05576efab7d3cc12d34073b76ce8104
Leopoldo Moreira <leopoldomoreira.lm@gmail.com>
2023-12-12 10:07:06 -0300

    Aula 22 - Componentização do Formulario

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,7 +1,15 @@
-function NewProject(){
+import styles from './NewProject.module.css'
+import ProjectForm from '../projects/ProjectForm'
+
+
+function Projects(){
     return(
-        <h1>NewProject</h1>
+        <div className={styles.project_container}> 
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjectForm btnText="Criar projeto"/>
+        </div>
     )
 }
 
-export default NewProject
+export default Projects
--- src/components/projects/ProjectForm.js
+++ src/components/projects/ProjectForm.js
@@ -1,18 +1,25 @@
-function ProjectForm(){
+import Input from '../form/Input';
+import Select from '../form/Select';
+import SubmitButton from '../form/SubmitButton';
+import styles from './ProjectForm.module.css'
+
+function ProjectForm({btnText}){
     return (
-        <form>
-            <div>
-                <input type="text" placeholder="Nome do projeto"/>
-            </div>
-            <div>
-                <input type="number" placeholder="Insira o orçamento"/>
-            </div>            
-            <select name="category_id">
-                <option disabled>Selecione a categoria</option>
-            </select>
-            <div>
-                <input type="submit" value="Criar projeto"/>
-            </div>
+        <form className={styles.form}>
+            <Input
+                type="text"
+                text="Nome do projeto"
+                name="name"
+                placeholder="Insira o nome do projeto"            
+            />
+            <Input
+                type="number"
+                text="Orçamento do projeto"
+                name="budget"
+                placeholder="Insira o orçamento total"            
+            />           
+            <Select type="category_id" text="Selecione a categoria"/>
+            <SubmitButton text={btnText}/>
         </form>
     )
     }
