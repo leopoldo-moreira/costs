f174bd6ca94f3797575d487a8ca3679ad885e76c
Leopoldo Moreira <leopoldomoreira.lm@gmail.com>
2023-12-15 08:41:46 -0300

    Aula 26 - Criando Dashboard

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,8 @@
 import {useLocation} from 'react-router-dom'
 import Message from "../layout/Message"
+import styles from './Projects.module.css'
+import Container from '../layout/Container'
+import LinkBtn from '../layout/LinkBtn'
 
 function Projects(){
 
@@ -11,10 +14,15 @@ function Projects(){
 
 
     return(
-        <div> 
-            <h1>Meus Projetos</h1>
-            {console.log(message)}
+        <div className={styles.project_container}> 
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkBtn to="/newproject" text="Criar Projeto"/>
+            </div>            
             { message && <Message type='success' msg={message}/>}
+            <Container customClass="start">
+                <p>Projetos...</p>
+            </Container>
         </div>
     )
 }
