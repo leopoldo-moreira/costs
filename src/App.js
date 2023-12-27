65ae5a7ea210e8ef8023a321523e4d31306d0b78
Leopoldo Moreira <leopoldomoreira.lm@gmail.com>
2023-12-27 09:51:20 -0300

    Aula 30 - Pagina de Edicao de Dados

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import Projects from './components/pages/Projects'
 import Contact from './components/pages/Contact'
 import Company from './components/pages/Company'
 import NewProject from './components/pages/NewProject'
-
 import Footer from './components/layout/Footer'
+import Project from './components/pages/Project'
 
 
 
@@ -24,7 +24,8 @@ function App() {
             <Route  path='/projects' element={<Projects/>}/>
             <Route  path='/company' element={<Company/>}/>
             <Route  path='/contact' element={<Contact/>}/>
-            <Route  path='/newproject' element={<NewProject/>}/>        
+            <Route  path='/newproject' element={<NewProject/>}/> 
+            <Route  path='/project/:id' element={<Project/>}/>      
         </Routes>
       </Container>
       <Footer/>
