902d71c8b0c14b515eb8d91de49d40d1eb8ffa01
Leopoldo Moreira <leopoldomoreira.lm@gmail.com>
2024-01-03 10:09:15 -0300

    Aula 36 -  Removendo serviços

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -72,7 +72,29 @@ function Project(){
         .catch(err => console.log(err))
     }
 
-    function removeService(){
+    function removeService(id, cost){
+        const serviceUpdated = project.services.filter(
+            (service) => service.id !== id
+        )
+
+        const projectUpdated = project
+
+        projectUpdated.services = serviceUpdated
+        projectUpdated.cost = parseFloat(projectUpdated.cost) - parseFloat(cost)
+
+        fetch(`http://localhost:5000/${projectUpdated.id}`,{
+            method: 'PATCH',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(projectUpdated)
+        })
+        .then(resp => resp.json())
+        .then((data) => {
+            setProject(projectUpdated)
+            setServices(serviceUpdated)
+            setMessage("Serviço removido com sucesso!")
+            setType('success')
+        })
+        .catch(err=> console.log(err))
 
     }
 
