af4e9746b813f7cf3843a95d27a925df5cddbb84
Leopoldo Moreira <leopoldomoreira.lm@gmail.com>
2024-03-12 22:37:08 -0300

    to responsive

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import NavBar from './components/layout/NavBar'
+import './index.css'
+
 
 import Container from './components/layout/Container'
 import Home from './components/pages/Home'
@@ -10,25 +12,21 @@ import NewProject from './components/pages/NewProject'
 import Footer from './components/layout/Footer'
 import Project from './components/pages/Project'
 
-
-
-
-
 function App() {
   return (
-    <Router>
-      <NavBar/>
-      <Container customClass="min-height">
-        <Routes>        
-            <Route exact path='/' element={<Home/>}/> 
-            <Route  path='/projects' element={<Projects/>}/>
-            <Route  path='/company' element={<Company/>}/>
-            <Route  path='/contact' element={<Contact/>}/>
-            <Route  path='/newproject' element={<NewProject/>}/> 
-            <Route  path='/project/:id' element={<Project/>}/>      
+    <Router className='router'>
+      <NavBar />
+      <Container className='router' style={{border: '1px solid blue'}} customClass="min-height">
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/company' element={<Company />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/newproject' element={<NewProject />} />
+          <Route path='/project/:id' element={<Project />} />
         </Routes>
       </Container>
-      <Footer/>
+      <Footer />
     </Router>
   );
 }
