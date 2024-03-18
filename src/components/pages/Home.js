fe9e736dbf8164552fc83e5efffd792750abbddd
Leopoldo Moreira <leopoldomoreira.lm@gmail.com>
2024-03-17 23:05:04 -0300

    dfds

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,7 +5,7 @@ import styles from "./Home.module.css"
 function Home(){
     return(
         <section className={styles.mainSection}>
-            <h1>Bem-vindo ddfdd ao <span>Costs</span></h1>
+            <h1>Bem-vindo ao <span>Costs</span></h1>
             <p>Comece a gerenciar os seus projetos agora mesmo!</p>
             <LinkBtn text ="Criar Projeto" to="/newproject"/>
             <img src={Savings} alt="Costs"/>
