ee4d5ff6c522a700c280948f941366067184af8c
Leopoldo Moreira <leopoldomoreira.lm@gmail.com>
2024-03-19 21:03:15 -0300

    To teste responsive fix bug

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,17 +4,7 @@ import styles from  './Footer.module.css'
 function Footer(){
     return (
         <footer className={styles.footer}>
-            <ul className={styles.social_list}>
-                <li className={styles.social_item}>
-                    <FaFacebook/>
-                </li>
-                <li className={styles.social_item}>
-                    <FaInstagram/>
-                </li>
-                <li className={styles.social_item}>
-                    <FaLinkedin/>
-                </li> 
-            </ul>
+            
             <p>
                 <span>Costs</span> &copy; 2023 
             </p>
